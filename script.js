3f1021e342d34b267b15af22c8e58b029783ae88
Zaid-ansari-shy <zaidansari082010@gmail.com>
2025-12-19 15:10:14 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function analyzePage() {
+  const url = document.getElementById("urlInput").value;
+
+  if (!url) {
+    alert("Please enter a URL");
+    return;
+  }
+
+  document.getElementById("result").classList.remove("hidden");
+
+  document.getElementById("purpose").innerText =
+    "Provide information or service related to the page";
+
+  document.getElementById("ymyl").innerText =
+    "Clear YMYL – Health / Safety";
+
+  document.getElementById("reputation").innerText =
+    "Positive reputation";
+
+  document.getElementById("mc").innerText =
+    "Satisfying Main Content";
+
+  document.getElementById("eeat").innerText =
+    "Trustworthy with adequate expertise";
+
+  document.getElementById("pq").innerText =
+    "High";
+}
 
